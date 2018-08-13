69062fd689f67f3a993ed71b9d4992750bd1ab26
Dale <Dale.Carr@qa.com>
2018-08-13 13:46:08 +0100

    Neatened Up webDriverTest

--- WebDriverTest/src/WebTest.java
+++ WebDriverTest/src/WebTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,15 +10,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTest {
+	static WebDriver myDriver;
 	
+	@BeforeClass
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dale\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	}
+	
+	@Before
+	public void setUp() {
+		 myDriver = new ChromeDriver();
+		myDriver.manage().window().maximize();
+	}
 	@Test
 	public void openGoogleTest() {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dale\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		WebDriver myDriver = new ChromeDriver();
-		myDriver.manage().window().maximize();
 		myDriver.get("http://www.google.co.uk");
-	WebElement myElement = 	myDriver.findElement(By.cssSelector("#tsf > div.tsf-p > div.jsb > center > input[type=\"submit\"]:nth-child(1)"));
-		assertTrue(myElement.isDisplayed());
+		WebElement myElement = 	myDriver.findElement(By.cssSelector("#tsf > div.tsf-p > div.jsb > center > input[type=\"submit\"]:nth-child(1)"));
+		assertTrue(myElement.isDisplayed());	
+	}
+	
+	@After
+	public void tearDown() {
 		myDriver.close();
 	}
 
